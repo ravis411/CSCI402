7967c971ad51099d155aa9091eca6e924539fb52
Ryan <ryanwdav@usc.edu>
2015-10-13 14:06:38 -0700

    Changed numPhysPages to 100

--- nachos-csci402/code/machine/machine.h
+++ nachos-csci402/code/machine/machine.h
@@ -34,7 +34,7 @@ using namespace std;
 					// the disk sector size, for
 					// simplicity
 
-#define NumPhysPages    50
+#define NumPhysPages    100
 #define MemorySize 	(NumPhysPages * PageSize)
 #define TLBSize		4		// if there is a TLB, make it small
 

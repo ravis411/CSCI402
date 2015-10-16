8f192a24820af3c5b91a2b8b29df9154b75ef2d0
ryanwdav <ryanwdav@usc.edu>
2015-10-16 00:57:36 -0700

    Forward Declaration for proccessTableEntry

--- nachos-csci402/code/threads/system.h
+++ nachos-csci402/code/threads/system.h
@@ -31,7 +31,7 @@ extern Interrupt *interrupt;			// interrupt status
 extern Statistics *stats;			// performance metrics
 extern Timer *timer;				// the hardware alarm clock
 extern BitMap *pageTableBitMap;	//Bitmap to track unused pages
-extern class ProcessTableEntry;
+class ProcessTableEntry;
 extern std::map<AddrSpace*, ProcessTableEntry*> processTable;	//The ProcessTable
 
 #ifdef USER_PROGRAM
